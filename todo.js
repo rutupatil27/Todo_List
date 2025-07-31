4fe6765475b1c1feddd706a3baac22ba4471ee1b
Rutuja Patil <167597523+rutupatil27@users.noreply.github.com>
2025-07-31 21:36:28 +0530

    Improvised edit feature, Removed default todo & Media queries

--- todo.js
+++ todo.js
@@ -2,9 +2,11 @@ let addbtn= document.querySelector('.newTodo');
 let header= document.querySelector('.header');
 let content= document.querySelector('.content');
 let input= document.querySelector('.enterTodo');
+let h4= document.querySelector('h4');
 let newTodo;
 
 addbtn.addEventListener("click",()=>{
+    h4.style.display="none";
     newTodo= addNew();
     if(!newTodo) return;
     let div= document.createElement('div');
@@ -30,7 +32,7 @@ addbtn.addEventListener("click",()=>{
 
 function addNew(){
     let todo= input.value.trim();
-    if(todo===""||todo.length>23){
+    if(todo.trim()===""||todo.length>23){
         alert("Invalid Todo OR too long Todo");
         input.value="";
         return null;
@@ -39,29 +41,52 @@ function addNew(){
     return todo;
 }
 
-function editTodo(){
-    newTodo=prompt("Edit the todo");
-    while(true){
-        if(newTodo.trim()===""||newTodo===null){
-            newTodo=prompt("Enter a valid todo");
-        }
-        else{
-            return false;
-        }
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addbtn.click();
     }
-};
+});
 
 content.addEventListener('click',function(event){
     if(event.target.className=='fa-solid fa-trash-can icon'){
         let parent= event.target.parentElement;
         parent.remove();
+        let alltodos= document.querySelectorAll('.list');
+        if(alltodos.length===0){
+            h4.style.display="block";
+        }
     }
 });
 
 content.addEventListener('click',function(event){
     if(event.target.className=='fa-solid fa-pen-to-square icon'){
-        editTodo();
         let prevlabel= event.target.previousElementSibling;
-        prevlabel.innerText=`${newTodo}`;
+        let currText= prevlabel.innerText;
+        let editinp= document.createElement('input');
+        editinp.type= "text";
+        editinp.value= `${currText}`;
+        editinp.id= "editinp";
+        prevlabel.replaceWith(editinp);
+        editinp.focus();
+        editinp.addEventListener('keypress',function(event){
+            if(event.key=="Enter"){
+                validation(editinp,prevlabel);
+            }
+        });
+        editinp.addEventListener('blur',function(event){
+            validation(editinp,prevlabel);
+        });
     }
 });
+
+function validation(editinp,prevlabel){
+    const updated = editinp.value.trim();
+    if (updated.trim() === "" || updated.length > 23) {
+        editinp.value="";
+        editinp.placeholder="Invalid input Or too long input";
+        editinp.focus();
+        return;
+    }
+    editinp.replaceWith(prevlabel);
+    prevlabel.innerText= `${editinp.value}`;
+}
